f6752429c7a51d09f4e23d5a5d6bcb688e6b57af
yangjian <1422731368@qq.com>
2024-04-04 00:17:06 +0800

    first commit

---
+++ chatbot-api-interfaces/src/main/java/org/example/chatbot/api/ApiApplication.java
@@ -0,0 +1,12 @@
+package org.example.chatbot.api;
+
+import org.springframework.boot.SpringApplication;
+
+/**
+ * 启动入口
+ */
+public class ApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+}
